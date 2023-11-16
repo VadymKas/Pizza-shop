6bc400cf620afab4242abf3a8189eb6169650f8c
Vadym Kashchyn <v.kaschin@gmail.com>
2023-11-16 19:51:04 +0200

    feat: Added fetch request

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import './scss/app.scss';
+import { useEffect, useState } from 'react';
 import Header from './components/Header';
 import Categories from './components/Categories';
 import Sort from './components/Sort';
 import PizzaBlock from './components/PizzaBlock';
 
-import pizzasDB from './assets/pizzas.json';
-
 function App() {
+  const [pizzasDB, setPizzasDB] = useState([]);
+
+  useEffect(() => {
+    fetch('https://65564cc384b36e3a431f897e.mockapi.io/pizzaDB')
+      .then((res) => res.json())
+      .then((res) => setPizzasDB(res))
+  }, []);
+
   return (
     <div className='App'>
       <div className='wrapper'>
